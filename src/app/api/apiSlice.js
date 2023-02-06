404b178a2a2b50c21b75f79a866b455bda546acf
Tiefu Zhu <tiefu.zhu@gmail.com>
2023-02-06 22:43:13 +1100

    update baseUrl

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://localhost:3500',
+    baseUrl: 'https://eng-req-tracker-api.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers, api) => {
         const accessToken = api.getState().auth.token
